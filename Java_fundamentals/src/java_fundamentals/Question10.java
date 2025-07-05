6063c45ee7788d8aa5f83016f859833978829575
Dedeepya <dedeepya678@gmail.com>
2025-07-05 13:02:57 +0530

    java fn

--- Java_fundamentals/src/java_fundamentals/Question10.java
+++ Java_fundamentals/src/java_fundamentals/Question10.java
@@ -3,9 +3,28 @@ import java.util.*;
 public class Question10 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String sc = new nextLine()
-				
+		System.out.println("Enter the gender:");
 
-	}
+				String gender=sc.nextLine();
+				int age=10;
+		     if(gender.equals("Female")){
+		    	 if(age>=1 && age<=58) {
+		    		 System.out.println("8.2%");
+		    	 }else if(age>=59 && age<=100) {
+		    		 System.out.println("9.2%");
+		    	 }
+		     }else if(gender.equals("Male")) {
+		    		 if(age>=1 && age<=58) {
+		    			 System.out.println("8.4%");
+		    		 }else if(age>=59 && age<=100) {
+		    			 System.out.println("10.5%");
+		    		 }
+		    	 }else {
+		    		 System.out.println("Invalid output");
+		    	 }
+		     
+			}
 
-}
+		}
+
+	
